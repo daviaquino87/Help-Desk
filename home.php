7d565e3fbb4b65b7565f7f0ffaa48de6f914150c
daviaquino87 <davi26031@gmail.com>
2022-09-01 10:22:43 -0300

    style(index home):style by index and homepage

---
+++ home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./public/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+
+    <title>home</title>
+</head>
+<body>
+    <div class="container">
+       <nav class="navBar">
+        <h4 class="span-nav">App Help Dask</h4>
+       </nav>
+
+       <div class="conteudo">
+        <h4 class="span-conteudo">Actions</h4>
+        <div class="action-box">
+            <button class="action">
+                <img src="./public/images/Frame 3.png" alt="">
+            </button>
+            <button class="action">
+                <img src="./public/images/Union.png" alt="">
+            </button>
+        </div>
+       </div>
+    </div>
+</body>
+</html>
