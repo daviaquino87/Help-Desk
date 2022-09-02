d299b1504260921c9f8b285aa974f219f3b2bc23
daviaquino87 <davi26031@gmail.com>
2022-09-01 23:54:49 -0300

    edit logout

--- public/views/home.php
+++ public/views/home.php
@@ -2,7 +2,7 @@
 session_start();
 
 if (!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] != 'SIM') {
-    header('Location: http://localhost/php/HelpDask/public/views/index.php?Error=1');
+    header('Location: http://localhost/php/HelpDask/public/views/index.php?Error');
 }
 
 ?>
--- services/logout.php
+++ services/logout.php
@@ -1,7 +1,5 @@
 <?php
 session_start();
-
-session_abort();
 session_destroy();
 
 header('Location: http://localhost/php/HelpDask/public/views/index.php');
